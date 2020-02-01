6a3d26bc940f829d3ff7f9c3952974df1f90a777
LenardMiller <leopard50x1@gmail.com>
2020-01-31 21:38:30 -0800

    added Tree Giant to spawn random

--- src/main/util/KeyBinds.java
+++ src/main/util/KeyBinds.java
@@ -121,28 +121,29 @@ public class KeyBinds {
     }
 
     private void spawnRandom() {
-        int r = (int) p.random(0,4.99f);
+        int r = (int) p.random(0,5.99f);
         PVector rp = randomSpawnPosition();
-        if (r == 0) {
-            enemies.add(new SmolBug(p, rp.x, rp.y));
-            enemies.get(enemies.size() - 1).requestPath(enemies.size() - 1);
+        switch (r) {
+            case 0:
+                enemies.add(new SmolBug(p, rp.x, rp.y));
+                break;
+            case 1:
+                enemies.add(new MidBug(p, rp.x, rp.y));
+                break;
+            case 2:
+                enemies.add(new BigBug(p, rp.x, rp.y));
+                break;
+            case 3:
+                enemies.add(new TreeSprite(p, rp.x, rp.y));
+                break;
+            case 4:
+                enemies.add(new TreeSpirit(p, rp.x, rp.y));
+                break;
+            case 5:
+                enemies.add(new TreeGiant(p, rp.x, rp.y));
+                break;
         }
-        if (r == 1) {
-            enemies.add(new MidBug(p, rp.x, rp.y));
-            enemies.get(enemies.size() - 1).requestPath(enemies.size() - 1);
-        }
-        if (r == 2) {
-            enemies.add(new BigBug(p, rp.x, rp.y));
-            enemies.get(enemies.size() - 1).requestPath(enemies.size() - 1);
-        }
-        if (r == 3) {
-            enemies.add(new TreeSprite(p, rp.x, rp.y));
-            enemies.get(enemies.size() - 1).requestPath(enemies.size() - 1);
-        }
-        if (r == 4) {
-            enemies.add(new TreeSpirit(p, rp.x, rp.y));
-            enemies.get(enemies.size() - 1).requestPath(enemies.size() - 1);
-        }
+        enemies.get(enemies.size() - 1).requestPath(enemies.size() - 1);
     }
 
     public void debugKeys() {
