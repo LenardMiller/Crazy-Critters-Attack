a564da274155ae0d8d2ef2d7ebed753fde948799
LenardMiller <leopard50x1@gmail.com>
2020-09-29 16:56:58 -0700

    remove todo

--- src/main/towers/turrets/Flamethrower.java
+++ src/main/towers/turrets/Flamethrower.java
@@ -187,7 +187,7 @@ public class Flamethrower extends Turret {
         upgradeSprites[3] = spritesH.get("metalWallTW");
     }
 
-    public void upgradeSpecial() { //todo: fix crash
+    public void upgradeSpecial() {
         if (nextLevelA == 0) flameTimer += 2;
         if (nextLevelA == 1) rotationSpeed += 0.02;
         if (nextLevelB == 1) {
