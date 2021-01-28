613fce3728c1fe481f9e7b251ddeb609227cacde
LenardMiller <leopard50x1@gmail.com>
2021-01-28 10:38:38 -0800

    added spikey glue icon

--- src/main/towers/turrets/Gluer.java
+++ src/main/towers/turrets/Gluer.java
@@ -172,7 +172,7 @@ public class Gluer extends Turret {
 
         upgradeIcons[3] = spritesAnimH.get("upgradeIC")[0];
         upgradeIcons[4] = spritesAnimH.get("upgradeIC")[8];
-        upgradeIcons[5] = spritesAnimH.get("upgradeIC")[0];
+        upgradeIcons[5] = spritesAnimH.get("upgradeIC")[26];
     }
 
     public void upgradeSpecial(int id) {
