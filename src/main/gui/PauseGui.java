f5be93aab14d1c9ace65d112a13a31a6a1445288
Leopard501 <leopard50x1@gmail.com>
2021-02-02 22:48:36 -0800

    added ResumeGame button

--- src/main/gui/PauseGui.java
+++ src/main/gui/PauseGui.java
@@ -2,12 +2,14 @@ package main.gui;
 
 import main.gui.guiObjects.buttons.ExitGame;
 import main.gui.guiObjects.buttons.RestartLevel;
+import main.gui.guiObjects.buttons.ResumeGame;
 import processing.core.PApplet;
 
 public class PauseGui {
 
     private final PApplet P;
 
+    public static ResumeGame resumeGame;
     public static RestartLevel restartLevel;
     public static ExitGame exitGame;
 
@@ -21,6 +23,7 @@ public class PauseGui {
     }
 
     public void display() {
+        resumeGame.main();
         restartLevel.main();
         exitGame.main();
     }
@@ -29,6 +32,7 @@ public class PauseGui {
      * Creates buttons
      */
     private void build() {
+        resumeGame = new ResumeGame(P, P.width/2f, (P.height/2f) - 50, "null", true);
         restartLevel = new RestartLevel(P, P.width/2f, P.height/2f, "null", true);
         exitGame = new ExitGame(P, P.width/2f, (P.height/2f) + 50, "null", true);
     }
---
+++ src/main/gui/guiObjects/buttons/ResumeGame.java
@@ -0,0 +1,50 @@
+package main.gui.guiObjects.buttons;
+
+import processing.core.PApplet;
+import processing.core.PVector;
+
+import static main.Main.*;
+
+public class ResumeGame extends Button {
+
+    public ResumeGame(PApplet p, float x, float y, String type, boolean active) {
+        super(p, x, y, type, active);
+        position = new PVector(x, y);
+        size = new PVector(200, 24);
+        spriteIdle = spritesAnimH.get("towerTabSwitchBT")[0];
+        spritePressed = spritesAnimH.get("towerTabSwitchBT")[1];
+        spriteHover = spritesAnimH.get("towerTabSwitchBT")[2];
+        sprite = spriteIdle;
+    }
+
+    public void main(){
+        hover();
+        display();
+    }
+
+    /**
+     * If mouse over, push in.
+     */
+    public void hover(){
+        if (p.mouseX < position.x+size.x/2 && p.mouseX > position.x-size.x/2 && p.mouseY < position.y+size.y/2 &&
+                p.mouseY > position.y-size.y/2 && alive) {
+            sprite = spriteHover;
+            if (inputHandler.leftMousePressedPulse) {
+                clickIn.stop();
+                clickIn.play(1, volume);
+            }
+            if (p.mousePressed && p.mouseButton == LEFT) sprite = spritePressed;
+            if (holdable && p.mousePressed && p.mouseButton == LEFT) action();
+            else if (inputHandler.leftMouseReleasedPulse) {
+                clickOut.stop();
+                clickOut.play(1, volume);
+                action();
+                sprite = spritePressed;
+            }
+        } else sprite = spriteIdle;
+    }
+
+    public void action() {
+        paused = false;
+    }
+}
