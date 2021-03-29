1ff9eb7d2d75dfa177a128d11d4baa73fd769949
Leopard501 <leopard50x1@gmail.com>
2021-03-29 11:40:56 -0700

    added some docs to Utilities

--- src/main/misc/Utilities.java
+++ src/main/misc/Utilities.java
@@ -403,6 +403,12 @@ public class Utilities {
         return input + p.random(-input * by, input * by);
     }
 
+    /**
+     * Stops and starts playing the given sound.
+     * @param sound sound to play, won't crash if null
+     * @param speed speed and to pitch to play at, 1 for normal
+     * @param volume volume to play at, 1 for normal
+     */
     public static void playSound(SoundFile sound, float speed, float volume) {
         if (sound != null) {
             sound.stop();
@@ -410,6 +416,12 @@ public class Utilities {
         }
     }
 
+    /**
+     * Plays the given sound with a 20% pitch and speed variation.
+     * @param p the PApplet
+     * @param sound sound to play, won't crash if null
+     * @param volume volume to play at, 1 for normal
+     */
     public static void playSoundRandomSpeed(PApplet p, SoundFile sound, float volume) {
         playSound(sound, p.random(0.8f, 1.2f), volume);
     }
