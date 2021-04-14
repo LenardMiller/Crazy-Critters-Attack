1bc270c8258799e91c429ef0884e509fde18343a
Leopard501 <leopard50x1@gmail.com>
2021-04-14 10:13:01 -0700

    fixed capitalization

--- src/main/towers/turrets/TeslaTower.java
+++ src/main/towers/turrets/TeslaTower.java
@@ -150,7 +150,7 @@ public class TeslaTower extends Turret {
         upgradeDescB[4] = "damage";
         upgradeDescC[4] = "";
 
-        upgradeDescA[5] = "vastly"; //todo: cap
+        upgradeDescA[5] = "Vastly";
         upgradeDescB[5] = "increase";
         upgradeDescC[5] = "firerate";
         //icons
