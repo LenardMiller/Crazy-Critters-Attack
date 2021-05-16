427c602bf76a9363159d937237aefa7f021b3907
Leopard501 <leopard50x1@gmail.com>
2021-05-16 11:59:27 -0700

    added IceTower vaportrail

--- src/main/towers/turrets/IceTower.java
+++ src/main/towers/turrets/IceTower.java
@@ -1,9 +1,11 @@
 package main.towers.turrets;
 
 import main.misc.Tile;
+import main.particles.RailgunBlast;
 import main.towers.IceWall;
 import main.towers.Wall;
 import processing.core.PApplet;
+import processing.core.PImage;
 import processing.core.PVector;
 
 import static main.Main.*;
@@ -15,6 +17,16 @@ public class IceTower extends Turret {
 
     private int wallHp;
     private int wallTimeUntilDamage;
+
+    private PImage[] vaporTrail;
+    private PImage[] vaporEndSprites;
+    private final int BETWEEN_VAPOR_FRAMES;
+    private int betweenVaporTimer;
+    private int currentVaporFrame;
+    private PVector vaporStart;
+    private float vaporAngle;
+    private int vaporLength;
+    private PVector vaporPartLength;
 
     public IceTower(PApplet p, Tile tile) {
         super(p, tile);
@@ -34,6 +46,13 @@ public class IceTower extends Turret {
         price = ICE_TOWER_PRICE;
         value = price;
 
+        BETWEEN_VAPOR_FRAMES = down60ToFramerate(3);
+        currentVaporFrame = 16;
+        betweenIdleFrames = 3;
+        betweenFireFrames = 3;
+        vaporTrail = animatedSprites.get("railgunVaporTrailTR");
+        vaporEndSprites = animatedSprites.get("railgunBlastPT");
+
         loadSprites();
         setUpgrades();
         spawnParticles();
@@ -42,6 +61,16 @@ public class IceTower extends Turret {
 
     @Override
     protected void spawnProjectiles(PVector position, float angle) {
+        particles.add(new RailgunBlast(p,position.x,position.y,0));
+        currentVaporFrame = 0;
+        vaporStart = position;
+        PVector vaporEnd = targetEnemy.position;
+        vaporAngle = angle;
+        float c = sqrt(sq(vaporEnd.x-vaporStart.x)+sq(vaporEnd.y-vaporStart.y));
+        vaporLength = (int)(c/24);
+        vaporPartLength = PVector.fromAngle(vaporAngle - radians(90));
+        vaporPartLength.setMag(24);
+
         targetEnemy.damageWithoutBuff(damage, this, "ice", PVector.fromAngle(angle), damage > 0);
 
         Tile tile = tiles.get((roundTo(targetEnemy.position.x, 50) / 50) + 1, (roundTo(targetEnemy.position.y, 50) / 50) + 1);
@@ -52,6 +81,48 @@ public class IceTower extends Turret {
             updateNodes();
             updateTowerArray();
         } else if (tile.tower instanceof IceWall) tile.tower.heal(1);
+    }
+
+    @Override
+    public void displayMain() {
+        //shadow
+        p.pushMatrix();
+        p.translate(tile.position.x - size.x / 2 + 2, tile.position.y - size.y / 2 + 2);
+        p.rotate(angle);
+        p.tint(0,60);
+        p.image(sprite,-size.x/2-offset,-size.y/2-offset);
+        p.tint(255);
+        p.popMatrix();
+        //vaporTrail
+        displayVaporTrail();
+        //main
+        p.pushMatrix();
+        p.translate(tile.position.x - size.x / 2, tile.position.y - size.y / 2);
+        p.rotate(angle);
+        p.tint(255, tintColor, tintColor);
+        p.image(sprite,-size.x/2-offset,-size.y/2-offset);
+        p.popMatrix();
+        p.tint(255);
+    }
+
+    private void displayVaporTrail() {
+        if (currentVaporFrame < vaporTrail.length) {
+            for (int i = 0; i <= vaporLength; i++) {
+                p.pushMatrix();
+                float x = vaporStart.x + (vaporPartLength.x*i);
+                float y = vaporStart.y + (vaporPartLength.y*i);
+                p.translate(x, y);
+                p.rotate(vaporAngle);
+                p.image(vaporTrail[currentVaporFrame], -2, 0);
+                if (i == vaporLength && currentVaporFrame < 11) p.image(vaporEndSprites[currentVaporFrame], -13, -15);
+                p.popMatrix();
+            }
+            if (betweenVaporTimer < BETWEEN_VAPOR_FRAMES) betweenVaporTimer++;
+            else {
+                currentVaporFrame++;
+                betweenVaporTimer = 0;
+            }
+        }
     }
 
     @Override
