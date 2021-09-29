c93a04ee6297a76bc5d7c5dbce3160aecf321875
blakebabb <leopard50x1@gmail.com>
2021-09-29 09:34:32 -0700

    improve decay buff display

--- src/main/buffs/Decay.java
+++ src/main/buffs/Decay.java
@@ -1,11 +1,13 @@
 package main.buffs;
 
 import main.enemies.Enemy;
+import main.particles.Floaty;
+import main.particles.MiscParticle;
 import main.towers.turrets.Turret;
 import processing.core.PApplet;
 import processing.core.PVector;
 
-import static main.Main.enemies;
+import static main.Main.*;
 import static main.misc.Utilities.secondsToFrames;
 
 public class Decay extends Buff {
@@ -18,6 +20,7 @@ public class Decay extends Buff {
         lifeDuration = secondsToFrames(duration); //frames
         particle = "decay";
         name = "decay";
+        particleChance = 4;
         this.DAMAGE = (int) damage;
         this.enId = enId;
         this.turret = turret;
@@ -29,5 +32,24 @@ public class Decay extends Buff {
         enemy.barAlpha = 255;
         enemy.damageWithoutBuff(DAMAGE,turret, "decay", new PVector(0,0), false);
         effectTimer = p.frameCount + effectDelay;
+    }
+
+    @Override
+    protected void display() {
+        if (enId < 0) buffs.remove(this);
+        else {
+            Enemy enemy = enemies.get(enId);
+            if (p.random(particleChance) < 1) {
+                topParticles.add(new MiscParticle(p,
+                        (float) (enemy.position.x + 2.5 + p.random((enemy.size.x / 2) * -1,
+                                (enemy.size.x / 2))), (float) (enemy.position.y + 2.5 + p.random((enemy.size.x / 2) * -1,
+                        (enemy.size.x / 2))), p.random(0, 360), particle));
+            } if (p.random(particleChance * 2f) < 1) {
+                topParticles.add(new Floaty(p,
+                        (float) (enemy.position.x + 2.5 + p.random((enemy.size.x / 2) * -1, (enemy.size.x / 2))),
+                        (float) (enemy.position.y + 2.5 + p.random((enemy.size.x / 2) * -1,
+                        (enemy.size.x / 2))), p.random(25, 35), "smokeCloud"));
+            }
+        }
     }
 }
